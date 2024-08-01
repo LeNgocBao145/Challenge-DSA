319c938df26be614379694822f619103009ec000
LeNgocBao145 <himagito@gmail.com>
2024-08-01 07:56:27 +0700

    fix read file to process name Korean North or South

--- Challenge.cpp
+++ Challenge.cpp
@@ -5,6 +5,9 @@
 
 using namespace std;
 
+//K Dimension
+const int k = 2;
+
 struct Cities
 {
     string name;
@@ -13,12 +16,59 @@ struct Cities
     int population;
 };
 
-void readFile(string fileName, vector<Cities>& list)
+struct Node{
+    Cities data;
+    Node* left;
+    Node* right;
+};
+
+Node* createNode(Cities data)
+{
+    Node* newNode = new Node;
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+void Insert(Node* &root, Cities newCity, int depth)
+{
+    if(root == NULL)
+    {
+        root = createNode(newCity);
+        return;
+    }
+
+    int align = depth % k;
+
+    if(align == 1)
+    {
+        if(newCity.lat < root->data.lat)
+        {
+            Insert(root->left, newCity, depth + 1);
+        }else
+        {
+            Insert(root->right, newCity, depth + 1);
+        }
+    }else if(align == 0)
+    {
+        if(newCity.lng < root->data.lng)
+        {
+            Insert(root->left, newCity, depth + 1);
+        }else
+        {
+            Insert(root->right, newCity, depth + 1);
+        }
+    }
+}
+
+Node* readFile(string fileName, vector<Cities>& list)
 {
     ifstream file(fileName);
     string buffer;
     
-
+    Node* root = NULL;
     if(file.is_open())
     {
         getline(file, buffer);
@@ -33,10 +83,26 @@ void readFile(string fileName, vector<Cities>& list)
             char s = ',';
             ss >> dumb.lat >> s >> dumb.lng >> s;
 
-            getline(ss, dumb.country, ',');
+            string tmp;
+            getline(ss, tmp, ',');
+            
+            //To check if name of country is more than 2 words 
+            if(tmp[0] == '"')
+            {
+                tmp.erase(0, 1);
+                string tmp2;
+                getline(ss, tmp2, ',');
+                tmp2.erase(tmp2.length() - 1, 1);
+                tmp += tmp2;
+                dumb.country = tmp;
+            }else{
+                dumb.country = tmp;
+            }
+            
             ss >> dumb.population;
 
-            list.push_back(dumb);
+            Insert(root, dumb, 1);
+            //list.push_back(dumb);
         }
     }else
     {
@@ -44,6 +110,8 @@ void readFile(string fileName, vector<Cities>& list)
     }
 
     file.close();
+
+    return root;
 }
 
 int showMenu()
@@ -74,15 +142,26 @@ int showMenu()
     return choice;
 }
 
+void NLR(Node* pRoot)
+{
+    if(pRoot == NULL)
+    {
+        return;
+    }
+
+    cout << pRoot->data.name << " " << pRoot->data.lat << " " << pRoot->data.lng << " " << pRoot->data.country << " " << pRoot->data.population << endl;
+
+    NLR(pRoot->left);
+    NLR(pRoot->right);
+}
+
 int main()
 {
     vector<Cities> list;
     string fileName = "worldcities-20210313-population-50000+.txt";
 
-    // for(int i = 0; i < list.size(); i++)
-    // {
-    //     cout << list[i].name << " " << list[i].lat << " " << list[i].lng << " " << list[i].country << " " << list[i].population << endl;
-    // }
+    Node* root = NULL;
+
 
     int choice;
 
@@ -98,7 +177,8 @@ int main()
                 return 0;
             case 1:
                 //Read already existed file and add items into KD tree
-                readFile(fileName, list);
+                root = readFile(fileName, list);
+                NLR(root);
                 break;
             // case 2:
             //     break;
